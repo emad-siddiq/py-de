f5702c9e82239a42469edb7768690c5b11af7ec9
Emad <emadsiddiq@berkeley.edu>
2024-08-31 11:43:31 -0700

    update build file

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,9 @@ set -e
 
 # Define output directory and binary names
 OUTPUT_DIR="dist"
-BACKEND_MAC_BINARY="backend_mac_x86_64"
-BACKEND_UBUNTU_X86_BINARY="backend_ubuntu_x86_64"
+BACKEND_MAC_ARM_BINARY="backend_mac_arm64"
 BACKEND_UBUNTU_ARM_BINARY="backend_ubuntu_aarch64"
-FRONTEND_MAC_BINARY="frontend_mac_x86_64"
-FRONTEND_UBUNTU_X86_BINARY="frontend_ubuntu_x86_64"
+FRONTEND_MAC_ARM_BINARY="frontend_mac_arm64"
 FRONTEND_UBUNTU_ARM_BINARY="frontend_ubuntu_aarch64"
 
 # Ensure the output directory exists
@@ -29,13 +27,10 @@ echo $(pwd)
 # Build for backend project
 echo "Building backend project..."
 
-# Cross-compile for macOS (x86_64)
-GOOS=darwin GOARCH=amd64 go build -o "../../$OUTPUT_DIR/$BACKEND_MAC_BINARY" 
+# Cross-compile for macOS (ARM64)
+GOOS=darwin GOARCH=arm64 go build -o "../../$OUTPUT_DIR/$BACKEND_MAC_ARM_BINARY" 
 
-# Cross-compile for Ubuntu (x86_64)
-GOOS=linux GOARCH=amd64 go build -o "../../$OUTPUT_DIR/$BACKEND_UBUNTU_X86_BINARY" 
-
-# Cross-compile for Ubuntu (aarch64)
+# Cross-compile for Ubuntu (ARM64)
 GOOS=linux GOARCH=arm64 go build -o "../../$OUTPUT_DIR/$BACKEND_UBUNTU_ARM_BINARY" 
 
 cd ../../frontend/src
@@ -43,13 +38,10 @@ cd ../../frontend/src
 # Build for frontend project
 echo "Building frontend project..."
 
-# Cross-compile for macOS (x86_64)
-GOOS=darwin GOARCH=amd64 go build -o "../../$OUTPUT_DIR/$FRONTEND_MAC_BINARY" 
+# Cross-compile for macOS (ARM64)
+GOOS=darwin GOARCH=arm64 go build -o "../../$OUTPUT_DIR/$FRONTEND_MAC_ARM_BINARY" 
 
-# Cross-compile for Ubuntu (x86_64)
-GOOS=linux GOARCH=amd64 go build -o "../../$OUTPUT_DIR/$FRONTEND_UBUNTU_X86_BINARY" 
-
-# Cross-compile for Ubuntu (aarch64)
+# Cross-compile for Ubuntu (ARM64)
 GOOS=linux GOARCH=arm64 go build -o "../../$OUTPUT_DIR/$FRONTEND_UBUNTU_ARM_BINARY"
 
 echo "Build completed. Binaries are located in $OUTPUT_DIR."
